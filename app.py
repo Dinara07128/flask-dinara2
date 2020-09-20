8ce9926833f25de59cf23c34355e64bfcf1458e9
mischanja <misha.kasko@gmail.com>
2020-09-20 21:31:07 +0200

    corrected db name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEM_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
