0aa75fde297b5f39230443fd6cdbae4676d72ec5
admin <dinara07128@gmail.com>
2020-09-21 15:16:06 +0300

    5 занятие. Удаление и редактирование

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/posts")
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
@@ -37,9 +42,32 @@ def post_detail(id):
     return render_template("post_detail.html", article=article)
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/posts/<int:id>/delete")
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении статьи произошла ошибка"
+
+
+@app.route("/posts/<int:id>/update", methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При редактировании статьи произошла ошибка"
+    else:
+        return render_template("post_update.html", article=article)
 
 
 @app.route("/create-article", methods=['POST', 'GET'])
